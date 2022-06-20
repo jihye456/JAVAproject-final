3b37d494b901e380156fcd131e9c6dc4bece3ab8
jihye456 <101807169+jihye456@users.noreply.github.com>
2022-06-20 21:40:25 +0900

    This is java study file.

---
+++ JAVAprac/src/JAVAprac/MemberDAO.java
@@ -0,0 +1,281 @@
+package JAVAprac;
+
+import java.sql.*;
+import java.util.Vector;
+ 
+import javax.swing.table.DefaultTableModel;
+
+public class MemberDAO {
+ 
+	private static final String DRIVER
+		= "com.mysql.jdbc.Driver";
+	private static final String URL
+		= "jdbc:mysql://localhost/dev";
+   
+	private static final String USER = "root";
+	private static final String PASS = "1234";
+	Member_List mList;
+   
+	public MemberDAO() {
+
+	}
+
+	public MemberDAO(Member_List mList){
+		this.mList = mList;
+		System.out.println("DAO=>"+mList);
+	}
+
+	//DB연결
+	public Connection getConn(){
+		Connection conn = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql:// localhost:3306/sampledb", "root", "1234");
+           
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
+
+	//회원 정보를 얻는 메소드
+	public MemberDTO getMemberDTO(String id){
+       MemberDTO dto = new MemberDTO();
+       Connection con = null;
+       PreparedStatement ps = null;
+       ResultSet rs = null; 
+
+       try {
+    	   con = getConn();
+    	   String sql = "select * from tb_member where id=?";
+    	   ps = con.prepareStatement(sql);
+    	   ps.setString(1, id);
+           
+    	   rs = ps.executeQuery();
+    	   
+    	   if(rs.next()){
+    		   dto.setId(rs.getString("id"));
+    		   dto.setPwd(rs.getString("Pwd"));
+    		   dto.setName(rs.getString("Name"));
+    		   dto.setTel(rs.getString("tel"));
+    		   dto.setAddr(rs.getString("addr"));
+    		   dto.setBirth(rs.getString("birth"));
+    		   dto.setJob(rs.getString("job"));
+    		   dto.setGender(rs.getString("gender"));
+    		   dto.setEmail(rs.getString("email"));
+    		   dto.setIntro(rs.getString("intro"));
+               
+    	   }
+       } catch (Exception e) {
+    	   e.printStackTrace();
+       }      
+       
+       return dto;    
+	}
+
+	public Vector getMemberList(){
+
+		Vector data = new Vector();
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try{
+           
+			con = getConn();
+			String sql = "select * from tb_member order by name asc";
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+
+			while(rs.next()){
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String tel = rs.getString("tel");
+				String addr = rs.getString("addr");
+				String birth = rs.getString("birth");
+				String gender = rs.getString("gender");
+				String job = rs.getString("job");
+				String email = rs.getString("email");
+				String intro = rs.getString("intro");
+
+				Vector row = new Vector();
+				row.add(id);
+				row.add(pwd);
+				row.add(name);
+				row.add(tel);
+				row.add(addr);
+				row.add(birth);
+				row.add(job);
+				row.add(gender);
+				row.add(email);
+				row.add(intro);
+				
+				data.add(row);             
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return data;
+	}
+
+	//회원 등록
+	public boolean insertMember(MemberDTO dto){
+		boolean ok = false;
+
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		try{
+			con = getConn();
+			String sql = "insert into tb_member(" +
+						"id,pwd,name,tel,addr,birth," +
+						"job,gender,email,intro) "+
+						"values(?,?,?,?,?,?,?,?,?,?)";
+
+			ps = con.prepareStatement(sql);
+			ps.setString(1, dto.getId());
+			ps.setString(2, dto.getPwd());
+			ps.setString(3, dto.getName());
+			ps.setString(4, dto.getTel());
+			ps.setString(5, dto.getAddr());
+			ps.setString(6, dto.getBirth());
+			ps.setString(7, dto.getJob());
+			ps.setString(8, dto.getGender());
+			ps.setString(9, dto.getEmail());
+			ps.setString(10, dto.getIntro());          
+			int r = ps.executeUpdate();
+
+			if(r>0){
+				System.out.println("가입 성공");   
+				ok=true;
+			}else{
+				System.out.println("가입 실패");
+			}
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+		return ok;
+	}
+
+	//회원정보 수정
+	public boolean updateMember(MemberDTO vMem){
+		System.out.println("dto="+vMem.toString());
+		boolean ok = false;
+		Connection con = null;
+		PreparedStatement ps = null;
+		try{
+
+			con = getConn();           
+			String sql = "update tb_member set name=?, tel=?, addr=?, birth=?, job=?, gender=?" +
+					", email=?,intro=? "+ "where id=? and pwd=?";
+
+			ps = con.prepareStatement(sql);
+
+			ps.setString(1, vMem.getName());
+			ps.setString(2, vMem.getTel());
+			ps.setString(3, vMem.getAddr());
+			ps.setString(4, vMem.getBirth());
+			ps.setString(5, vMem.getJob());
+			ps.setString(6, vMem.getGender());
+			ps.setString(7, vMem.getEmail());
+			ps.setString(8, vMem.getIntro());
+			ps.setString(9, vMem.getId());
+			ps.setString(10, vMem.getPwd());
+
+			int r = ps.executeUpdate();
+
+			if(r>0) ok = true;
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+		return ok;
+	}
+
+	public boolean deleteMember(String id, String pwd){
+
+		boolean ok =false ;
+		Connection con =null;
+		PreparedStatement ps =null;
+
+		try {
+			con = getConn();
+			String sql = "delete from tb_member where id=? and pwd=?";
+
+			ps = con.prepareStatement(sql);
+			ps.setString(1, id);
+			ps.setString(2, pwd);
+			int r = ps.executeUpdate();
+
+			if (r>0) ok=true;
+
+		} catch (Exception e) {
+			System.out.println(e + "-> 오류발생");
+		}      
+		return ok;
+	}
+
+	public void userSelectAll(DefaultTableModel model) {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = getConn();
+			String sql = "select * from tb_member order by name asc";
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+
+			for (int i = 0; i < model.getRowCount();) {
+				model.removeRow(0);
+			}
+ 
+			while (rs.next()) {
+				Object data[] = { rs.getString(1), rs.getString(2),
+						rs.getString(3), rs.getString(4),
+						rs.getString(5),
+						rs.getString(6),
+						rs.getString(7),
+						rs.getString(8),
+						rs.getString(9),
+						rs.getString(10)};
+ 
+					model.addRow(data);                
+			}
+ 
+		} catch (SQLException e) {
+			System.out.println(e + "=> userSelectAll fail");
+		} finally{
+
+			if(rs!=null)
+				try {
+					rs.close();
+				} catch (SQLException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
+			if(ps!=null)
+				try {
+					ps.close();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			if(con!=null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        }
+	}
+}
---
+++ JAVAprac/src/JAVAprac/Member_List.java
@@ -0,0 +1,105 @@
+package JAVAprac;
+
+import java.awt.BorderLayout;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+ 
+ 
+public class Member_List extends JFrame implements MouseListener,ActionListener{
+
+	Vector v;  
+	Vector cols;
+	DefaultTableModel model;
+	JTable jTable;
+	JScrollPane pane;
+	JPanel pbtn;
+	JButton btnInsert;
+
+	public Member_List(){
+		super("주소록 관리 프로그램");
+		MemberDAO dao = new MemberDAO();
+		v = dao.getMemberList();
+		System.out.println("v="+v);
+		cols = getColumn();
+		model = new DefaultTableModel(v, cols);
+
+		jTable = new JTable(model);
+		pane = new JScrollPane(jTable);
+		add(pane);
+
+		pbtn = new JPanel();
+		btnInsert = new JButton("회원가입");
+		pbtn.add(btnInsert);
+		add(pbtn,BorderLayout.NORTH);
+		
+		jTable.addMouseListener(this);
+		btnInsert.addActionListener(this); 
+
+		setSize(600,200);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public Vector getColumn(){
+		Vector col = new Vector();
+		col.add("아이디");
+		col.add("비밀번호");
+		col.add("이름");
+		col.add("전화");
+		col.add("주소");
+		col.add("생일");
+		col.add("직업");
+		col.add("성별");
+		col.add("이메일");
+		col.add("자기소개");
+
+		return col;
+	}
+
+	public void jTableRefresh(){
+		
+		MemberDAO dao = new MemberDAO();
+		DefaultTableModel model= new DefaultTableModel(dao.getMemberList(), getColumn());
+		jTable.setModel(model);    
+	}
+	
+	public static void main(String[] args) {
+		new Member_List();
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		int r = jTable.getSelectedRow();
+		String id = (String) jTable.getValueAt(r, 0);
+		MemberProc mem = new MemberProc(id,this);         
+	}
+	
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub  
+	}
+	
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+    
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub  
+	}
+    
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == btnInsert ){
+			new MemberProc(this);
+		}  
+	} 
+}
